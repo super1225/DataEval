828d934784f0373222b43ce521eb4837a5f9a598
郭佳源 <guojiayuan@guojiayuandeMacBook-Air.local>
2025-05-24 15:02:24 +0800

    改动rule.py

--- quality_filter/iterator/__init__.py
+++ quality_filter/iterator/__init__.py
@@ -3,6 +3,6 @@ from .flow_control import Fork, Chain, If, IfElse, While, Aggregate
 from .field_based import (Select, SelectVal, AddFields, RemoveFields, ReplaceFields, MergeFields, RenameFields,
                           CopyFields,
                           InjectField, ConcatFields, ConcatArray, RemoveEmptyOrNullFields)
-from .rule import Character, EndWithTerminal
+from .rule import Character, EndWithTerminal,EndWithEllipsis,WordNumber,SentenceNumber,CheckNullValues,CheckUniqueValues,CheckDuplicateValues,ValidateFormat,ValidateDate,ValidateEmail,ValidatePhone,ValidatePostcode,ValidateIDCard,ValidateIPAddress
 from .score import Comprehensive
 from .transform import CSVToJSONConverter
